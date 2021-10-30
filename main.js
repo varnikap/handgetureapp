329ca739a6c8bc40a24816419ac39e1c34b2be3e
varnikap <78656240+varnikap@users.noreply.github.com>
2021-10-30 12:16:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,78 @@
 Webcam.set({
-width:350,
-height:300,
-image_format:'png',
-png_quality:90
-});
-camera = document.getElementById("camera");
- Webcam.attach('#camera');
-
- function takesnapshot()
- {
-     Webcam.snap(function(data_uri){
-         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-     });
- }
-
- console.log('ml5 version', ml5.version);
-
- classifer = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/yRh2qcVeH/model.json",modelLoaded);
-function modelLoaded()
-{
-    console.log('Model Loaded!');
-}
- function prediction()
- {
-     img = document.getElementById('captured_image');
-     classifer.classify(img,gotResult);
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+    });
+    camera = document.getElementById("camera");
+     Webcam.attach('#camera');
     
- }
- function gotResult(error,results)
- {
-     if(results)
+     function take_snapshot()
      {
-        
-         console.log(results);
+         Webcam.snap(function(data_uri){
+             document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+         });
      }
-     else
+    
+     console.log('ml5 version', ml5.version);
+    
+     classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yRh2qcVeH/model.json",modelLoaded);
+    function modelLoaded()
+    {
+        console.log('Model Loaded!');
+    }
+     function prediction()
      {
-        console.log(results);
+         img = document.getElementById('captured_image');
+         classifier.classify(img,gotResult);
         
      }
- }
-
- function speak()
-{
-var synth = window.speechSynthesis;
-gesture_data = "The gesture is " + prediction_1;
-var utterthis = new SpeechSynthesisUtterance(gesture_data);
-synth.speak(utterthis);
-}
+     function gotResult(error,results)
+     {
+         if(error)
+         {
+            
+             console.log(error);
+         }
+         else
+         {
+            console.log(results);
+        
+            document.getElementById("result_object_name").innerHTML = results[0].label;
+        
+            gesture = results[0].label;
+            
+            toSpeak = "";
+            
+            if(gesture == "amazing")
+            {
+              toSpeak = "This is looking amazing";
+              document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;"
+            }
+            else if(gesture == "best")
+            {
+              toSpeak = "All the best";
+              document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;"
+            }
+            else if(gesture == "victory")
+            {
+              toSpeak = "That was the marvelous victory";
+              document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;"
+            }
+        
+            speak();
+           
+            
+         }
+     }
+    
+     function speak()
+    {
+        var synth = window.speechSynthesis;
+    
+        speak_data = toSpeak;
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
