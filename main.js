84a3270429f9c0644f70f24cc4f62cc9ba5e7730
varnikap <78656240+varnikap@users.noreply.github.com>
2021-10-30 21:44:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+
 Webcam.set({
     width:350,
     height:300,
     image_format:'png',
     png_quality:90
     });
+
     camera = document.getElementById("camera");
-     Webcam.attach('#camera');
+
+     Webcam.attach( '#camera' );
     
      function take_snapshot()
      {
@@ -14,60 +17,53 @@ Webcam.set({
          });
      }
     
-     console.log('ml5 version', ml5.version);
+     console.log('ml5 version: ', ml5.version);
     
-     classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yRh2qcVeH/model.json",modelLoaded);
-    function modelLoaded()
-    {
+     classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+     function modelLoaded() {
         console.log('Model Loaded!');
-    }
-     function prediction()
-     {
-         img = document.getElementById('captured_image');
-         classifier.classify(img,gotResult);
-        
-     }
-     function gotResult(error,results)
-     {
-         if(error)
-         {
-            
-             console.log(error);
-         }
-         else
-         {
-            console.log(results);
-        
-            document.getElementById("result_object_name").innerHTML = results[0].label;
-        
-            gesture = results[0].label;
-            
-            toSpeak = "";
-            
-            if(gesture == "amazing")
-            {
-              toSpeak = "This is looking amazing";
-              document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;"
-            }
-            else if(gesture == "best")
-            {
-              toSpeak = "All the best";
-              document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;"
-            }
-            else if(gesture == "victory")
-            {
-              toSpeak = "That was the marvelous victory";
-              document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;"
-            }
-        
-            speak();
-           
-            
-         }
-     }
+      }
+          
+      function check()
+      {
+        img = document.getElementById('captured_image');
+        classifier.classify(img, gotResult);
+      }
     
-     function speak()
-    {
+    function gotResult(error, results) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(results);
+        
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+    
+        gesture = results[0].label;
+        
+        toSpeak = "";
+        
+        if(gesture == "amazing")
+        {
+          toSpeak = "This is looking amazing";
+          document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
+        }
+        else if(gesture == "best")
+        {
+          toSpeak = "All the best";
+          document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+        }
+        else if(gesture == "victory")
+        {
+          toSpeak = "That was the marvelous victory";
+          document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+        }
+    
+        speak();
+      }
+    }
+    
+    
+    function speak(){
         var synth = window.speechSynthesis;
     
         speak_data = toSpeak;
@@ -75,4 +71,5 @@ Webcam.set({
         var utterThis = new SpeechSynthesisUtterance(speak_data);
     
         synth.speak(utterThis);
+    
     }
